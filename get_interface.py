24e2c8da1081ebb04799d55cdfa132539ed46df2
Luiz Felipe Santana <luizfrsantana@gmail.com>
2025-03-06 03:17:38 +0000

    First Commit

---
+++ get_interface.py
@@ -0,0 +1,24 @@
+from napalm import get_network_driver
+import json
+
+with open("keys.txt", 'r') as file:
+    dados = json.load(file)
+
+hostname = "192.168.56.1" + input("Device IP - 192.168.56.1X: ")
+username = dados["username"]
+password = dados["password"]
+
+driver = get_network_driver("ios")
+
+device = driver(hostname=hostname, username=username, password=password)
+
+device.open()
+
+interfaces = device.get_interfaces_ip()
+
+for interface, details in interfaces.items():
+    print(f'Interface: {interface}')
+    for key, value in details.items():
+        print(f'{key}: {value}')
+
+device.close()
