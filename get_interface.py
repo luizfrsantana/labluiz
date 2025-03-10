078b775f57eaea9269c85d3145c5c4107f2470f5
Luiz Felipe Santana <luizfrsantana@gmail.com>
2025-03-10 18:39:11 +0000

    up

--- get_interface.py
+++ get_interface.py
@@ -28,13 +28,13 @@ def get_device_interface_info(hostname):
     # Get IP information of interfaces and details
     interface_ips = device.get_interfaces_ip()
     interfaces_info = device.get_interfaces()
-    clear_interface_data = []
+    sanitize_interface_data = []
 
     # Process interface data
     for interface_name, details in interface_ips.items():
         interface_data = {interface_name: {}}
         interface_data[interface_name]["description"] = interfaces_info[interface_name]['description']
-        interface_data[interface_name]["up"] = interfaces_info[interface_name]['is_up']
+        interface_data[interface_name]["status"] = "up" if interfaces_info[interface_name]['is_up'] else "down"
         interface_data[interface_name]["enable"] = interfaces_info[interface_name]['is_enabled']
 
         for key, value in details.items():
@@ -43,12 +43,12 @@ def get_device_interface_info(hostname):
             for ip, subnet in value.items():
                 interface_data[interface_name][key][ip] = subnet['prefix_length']
             
-        clear_interface_data.append(interface_data)
+        sanitize_interface_data.append(interface_data)
     
     # Close the connection with the device
     device.close()
     
-    return clear_interface_data
+    return sanitize_interface_data
 
 # Example of how to call the function in another code:
 hostname = "192.168.56.1" + input("Device IP - 192.168.56.1X: ")
