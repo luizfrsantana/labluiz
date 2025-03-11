e629e07150491173471e0b2ff5b1ffcb418143b6
Luiz Felipe Santana <luizfrsantana@gmail.com>
2025-03-11 21:37:23 +0000

    update code with comments

--- connect_to_device_cisco.py
+++ connect_to_device_cisco.py
@@ -2,10 +2,22 @@ from napalm import get_network_driver
 from load_authentication_data import load_authentication_data
 
 def connect_to_device_cisco(hostname):
+    """
+    Function to connect to a Cisco device using the NAPALM library.
+    
+    Args:
+        hostname (str): The IP address or hostname of the Cisco device.
+    
+    Returns:
+        device: A device object representing the connection to the Cisco device.
+                This object can be used to execute commands and retrieve information.
+    """
+    
     # Define the driver for the device
     driver = get_network_driver("ios")
 
-    # Connect to the device
+    # Connect to the device using the provided hostname and authentication data
     device = driver(hostname=hostname, username=load_authentication_data()["username"], password=load_authentication_data()["password"])
 
+    # Return the device connection object
     return device
--- load_authentication_data.py
+++ load_authentication_data.py
@@ -1,12 +1,21 @@
 import json
 
 def load_authentication_data():
-
-    # Load authentication data
+    """
+    Function to load authentication data (username and password) from a JSON file.
+    
+    Returns:
+        dict: A dictionary containing the username and password loaded from the file.
+              Example: {"username": "user", "password": "pass"}
+    """
+    
+    # Load authentication data from the JSON file
     with open("data/keys.txt", 'r') as file:
         dados = json.load(file)
 
+    # Extract username and password from the loaded data
     username = dados["username"]
     password = dados["password"]
 
-    return {"username":username, "password":password }
+    # Return the authentication data as a dictionary
+    return {"username": username, "password": password}
